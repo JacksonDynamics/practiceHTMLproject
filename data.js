7e7c47ad41f415a1795707df4e6935bef8e5b96a
studmanj <bjackso3@hotmail.com>
2022-11-19 01:25:08 -0700

    Home Page map layout

--- data.js
+++ data.js
@@ -1,5 +1,6 @@
 const data = [
     {
+        id: 0,
         city: "Salt Lake",
         map: '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d118109.1232942954!2d-111.96976901005317!3d40.76783671757657!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87523d9488d131ed%3A0x5b53b7a0484d31ca!2sSalt%20Lake%20City%2C%20UT!5e0!3m2!1sen!2sus!4v1668810046579!5m2!1sen!2sus" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>',
         projects: {
@@ -22,7 +23,8 @@ const data = [
             }
     },
     {
-        city: "Columbus, Ohio",
+        id: 1,
+        city: "Columbus",
         map: '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d32161.19350201481!2d-82.96196492464534!3d40.01927770383598!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x883889c1b990de71%3A0xe43266f8cfb1b533!2sColumbus%2C%20OH!5e0!3m2!1sen!2sus!4v1668812493802!5m2!1sen!2sus" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>',
         projects: {
                 downtown: {
@@ -46,6 +48,7 @@ const data = [
             }
     },
     {
+        id:2,
         city: "Baltimore",
         map: '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d49425.02772996056!2d-76.65463763744636!3d39.26410916055061!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c80338480c322b%3A0xb56812f7525b5d39!2sBaltimore%2C%20MD%2021230!5e0!3m2!1sen!2sus!4v1668815286102!5m2!1sen!2sus" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>',
         Neighborhood: "Federal Hill",
@@ -63,6 +66,27 @@ const data = [
                     constructionStart: "Mar 19, 2022",
                     completionEnd: "Nov 11, 2022",
                     budget: "$400K"
+                },
+            }
+    },
+    {
+        id:3,
+        city: "Winston-Salem",
+        map: <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d206306.89579173698!2d-80.2434579!3d36.10457199999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8853a19b82d6b277%3A0x50325f54b43f8245!2sWinston-Salem%2C%20NC!5e0!3m2!1sen!2sus!4v1668828141334!5m2!1sen!2sus" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>,
+        projects: {
+                downtown: {
+                    projectNo: 87247,
+                    address: "175 N. Chestnut Street",
+                    constructionStart: "February 12, 2021",
+                    completionEnd: "November 7, 2023",
+                    budget: "$2.4M"
+                },
+                rural: {
+                    projectNo: 1243,
+                    address: "1489 Colony Lodge St.",
+                    constructionStart: "April 10, 2022",
+                    completionEnd: "August 17, 2024",
+                    budget: "$3,1M"
                 },
             }
     },
--- index.js
+++ index.js
@@ -4,11 +4,10 @@
 
 function show() {
     
-    /* Access image by id and change
-    the display property to block*/
+
     document.getElementById('IDmap')
             .style.display = "block";
 
-    // document.getElementById('btnID')
-    //         .style.display = "none";
+    document.getElementById('btnID')
+            .style.display = "none";
 }
