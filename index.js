0965a2bbc7dbc52b27ca68386a27d1a990557d7f
studmanj <bjackso3@hotmail.com>
2022-11-19 08:35:28 -0700

    Salem data

--- index.js
+++ index.js
@@ -94,27 +94,26 @@ const data = [
                     },
                 }
         },
-        // {
-        //     id:4,
-        //     city: "Winston-Salem",
-        //     map: <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d206306.89579173698!2d-80.2434579!3d36.10457199999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8853a19b82d6b277%3A0x50325f54b43f8245!2sWinston-Salem%2C%20NC!5e0!3m2!1sen!2sus!4v1668828141334!5m2!1sen!2sus" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>,
-        //     projects: {
-        //             downtown: {
-        //                 projectNo: 87247,
-        //                 address: "175 N. Chestnut Street",
-        //                 start: "February 12, 2021",
-        //                 end: "November 7, 2023",
-        //                 budget: "$2.4M"
-        //             },
-        //             rural: {
-        //                 projectNo: 1243,
-        //                 address: "1489 Colony Lodge St.",
-        //                 start: "April 10, 2022",
-        //                 end: "August 17, 2024",
-        //                 budget: "$3,1M"
-        //             },
-        //         }
-        // },
+        {
+            id:4,
+            city: "Winston-Salem",
+            projects: {
+                    downtown: {
+                        projectNo: 87247,
+                        address: "175 N. Chestnut Street",
+                        start: "February 12, 2021",
+                        end: "November 7, 2023",
+                        budget: "$2.4M"
+                    },
+                    rural: {
+                        projectNo: 1243,
+                        address: "1489 Colony Lodge St.",
+                        start: "April 10, 2022",
+                        end: "August 17, 2024",
+                        budget: "$3,1M"
+                    },
+                }
+        },
     ]
     
 //     console.log(data[0].projects.downtown.projectNo)
