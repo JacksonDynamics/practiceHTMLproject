148edc3db1454ee58ecce022f71a31965ef57613
studmanj <bjackso3@hotmail.com>
2022-11-19 05:49:18 -0700

    data

--- data.js
+++ data.js
@@ -48,10 +48,35 @@ const data = [
             }
     },
     {
-        id:2,
-        city: "Baltimore",
-        map: '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d49425.02772996056!2d-76.65463763744636!3d39.26410916055061!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c80338480c322b%3A0xb56812f7525b5d39!2sBaltimore%2C%20MD%2021230!5e0!3m2!1sen!2sus!4v1668815286102!5m2!1sen!2sus" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>',
-        Neighborhood: "Federal Hill",
+        id: 2,
+        city: "Columbus",
+        map: '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d32161.19350201481!2d-82.96196492464534!3d40.01927770383598!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x883889c1b990de71%3A0xe43266f8cfb1b533!2sColumbus%2C%20OH!5e0!3m2!1sen!2sus!4v1668812493802!5m2!1sen!2sus" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>',
+        projects: {
+                downtown: {
+                    projectNo: 1091-52, 
+                    address: "465 East Hudson St", 
+                    constructionStart: "March 2022",
+                    completionEnd: "September 2024",
+                    budget: "$1.5M",
+                    contact: "Hannah Webber",
+                    phone: "614-645-2859"
+                },
+                rural: {
+                    projectNo: 2269-08, 
+                    address: "1900 E Weber Rd", 
+                    constructionStart: "April 2023",
+                    completionEnd: "October 2026",
+                    budget: "$3.5M",
+                    contact: "John Merrill",
+                    phone: "614-746-5159"
+                },
+            }
+    },
+    {
+        id:3,
+        city: "Raleigh",
+        map: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d51911.35312006114!2d-78.66236503472645!3d35.77520499047369!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89ac5a2f9f51e0f7%3A0x6790b6528a11f0ad!2sRaleigh%2C%20NC!5e0!3m2!1sen!2sus!4v1668850210839!5m2!1sen!2sus",
+        neighborhood: "Federal Hill",
         projects: {
                 downtown: {
                     projectNo: 87247,
@@ -70,7 +95,7 @@ const data = [
             }
     },
     {
-        id:3,
+        id:4,
         city: "Winston-Salem",
         map: <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d206306.89579173698!2d-80.2434579!3d36.10457199999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8853a19b82d6b277%3A0x50325f54b43f8245!2sWinston-Salem%2C%20NC!5e0!3m2!1sen!2sus!4v1668828141334!5m2!1sen!2sus" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>,
         projects: {
