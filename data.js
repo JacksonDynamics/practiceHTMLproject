65c1081abdb17449393d49e0817b2d4f0cedfbb0
studmanj <bjackso3@hotmail.com>
2022-11-18 17:48:31 -0700

    Submit Button Functionality

--- data.js
+++ data.js
@@ -1,6 +1,49 @@
 const data = [
     {
         city: "Salt Lake",
-        map: 'https://arcg.is/0G9fzv'
-    }
+        map: '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d118109.1232942954!2d-111.96976901005317!3d40.76783671757657!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87523d9488d131ed%3A0x5b53b7a0484d31ca!2sSalt%20Lake%20City%2C%20UT!5e0!3m2!1sen!2sus!4v1668810046579!5m2!1sen!2sus" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>',
+        projects: {
+                downtown: {
+                    projectNo: 2021-87,
+                    address: "Sunnyside Ave & Foothill DrSalt Lake City, UT 84108", 
+                    constructionStart: "June 13, 2022",
+                    completionEnd: "Fall of 2023",
+                    budget: "$2.5M"
+                },
+                rural: {
+                    projectNo: 1243, 
+                    address: "300 W/ North Temple (NB), Salt Lake City, UT 84103", 
+                    constructionStart: "Sept. 2022",
+                    completionEnd: "End of 2022",
+                    budget: "$3.2M", 
+                    contact: "Dang Quach",
+                    phone: 801-535-6696
+                },
+            }
+    },
+    {
+        city: "Columbus, Ohio",
+        map: '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d32161.19350201481!2d-82.96196492464534!3d40.01927770383598!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x883889c1b990de71%3A0xe43266f8cfb1b533!2sColumbus%2C%20OH!5e0!3m2!1sen!2sus!4v1668812493802!5m2!1sen!2sus" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>',
+        projects: {
+                downtown: {
+                    projectNo: 1091-52, 
+                    address: "465 East Hudson St", 
+                    constructionStart: "March 2022",
+                    completionEnd: "September 2024",
+                    budget: "$1.5M",
+                    contact: "Hannah Webber",
+                    phone: "614-645-2859"
+                },
+                rural: {
+                    projectNo: 2269-08, 
+                    address: "1900 E Weber Rd", 
+                    constructionStart: "April 2023",
+                    completionEnd: "October 2026",
+                    budget: "$3.5M",
+                    contact: "John Merrill",
+                    phone: "614-746-5159"
+                },
+            }
+    },
 ]
+
--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-let El = document.querySelector("#sum-el");
-let cardsEl = document.querySelector("#cards-el");
+// let El = document.querySelector("#sum-el");
+// let cardsEl = document.querySelector("#cards-el");
+
+
+function show() {
+    
+    /* Access image by id and change
+    the display property to block*/
+    document.getElementById('IDmap')
+            .style.display = "block";
+
+    // document.getElementById('btnID')
+    //         .style.display = "none";
+}
